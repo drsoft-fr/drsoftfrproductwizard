fcb492c1d0d87b9002880d6b4748a620823dd170
Dylan Ramos <dylan.ramos.n@gmail.com>
2025-07-24 13:10:09 +0200

    Add Vue component and initialize front-end configurator rendering logic
    Implemented `App.vue` component for configurator entry point. Updated `main.js` to dynamically mount Vue apps to elements based on data attributes. Enhanced modularity and streamlined configurator initialization.

--- _dev/src/js/front/configurator/main.js
+++ _dev/src/js/front/configurator/main.js
@@ -1,1 +1,20 @@
 import '@/css/front/configurator/main.scss'
+
+import { createApp } from 'vue'
+import App from '@/vue/front/configurator/App.vue'
+
+const wizardElms = document.querySelectorAll(
+  '.js-drsoft-fr-product-wizard[data-configurator]',
+)
+
+wizardElms.forEach((elm) => {
+  const id = parseInt(elm.dataset.configurator || '')
+
+  if (!id || isNaN(id)) {
+    return
+  }
+
+  const app = createApp(App, { id })
+
+  app.mount(elm)
+})
---
+++ _dev/src/vue/front/configurator/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+const props = defineProps({
+  id: { type: Number, required: true }
+})
+</script>
+
+<template>
+  <div :id="'configurator-' + id" class="product-wizard-container"></div>
+</template>
+
+<style scoped lang="scss"></style>
