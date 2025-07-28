9ea51a732262dd8f83ab9b74f5a4b8fbb853a115
Dylan Ramos <dylan.ramos.n@gmail.com>
2025-07-28 16:17:10 +0200

    Add `ConfiguratorNotFoundException` in `drsoftfrproductwizard` module

---
+++ src/Exception/Configurator/ConfiguratorNotFoundException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace DrSoftFr\Module\ProductWizard\Exception\Configurator;
+
+/**
+ * Is thrown when Configurator is not found
+ */
+class ConfiguratorNotFoundException extends ConfiguratorException
+{
+}
