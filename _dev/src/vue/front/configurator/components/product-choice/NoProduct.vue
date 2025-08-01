05eebb9cc35240c3b3bab7c23729ca289064520b
Dylan Ramos <dylan.ramos.n@gmail.com>
2025-08-01 03:15:56 +0200

    Update `NoProduct` component to replace image with placeholder, add styling for improved consistency and visuals.

--- _dev/src/vue/front/configurator/components/product-choice/NoProduct.vue
+++ _dev/src/vue/front/configurator/components/product-choice/NoProduct.vue
@@ -3,13 +3,14 @@ import Action from '@/vue/front/configurator/components/product-choice/Action.vu
 
 const props = defineProps({
   choice: { type: Object, required: true },
-  noPictureImage: { type: Object, required: true },
 })
 </script>
 
 <template>
   <div class="card">
-    <img :src="noPictureImage.medium.url" alt="" class="card-img-top" />
+    <div class="img-placeholder-container">
+      <div class="img-placeholder"></div>
+    </div>
     <div class="card-body">
       <h4 class="card-text">{{ choice.label }}</h4>
       <Action @click="$emit('select', choice)" />
@@ -26,6 +27,16 @@ const props = defineProps({
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+  height: 100%;
+}
+
+.img-placeholder-container {
+  background-color: var(--bs-dark-bg-subtle);
+  height: 100%;
+}
+
+.img-placeholder {
+  background: var(--bs-gradient);
   height: 100%;
 }
 </style>
