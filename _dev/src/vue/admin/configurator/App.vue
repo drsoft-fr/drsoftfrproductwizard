977e9552b159a33bbc7e3204e124bb5eec37a5c4
Dylan Ramos <dylan.ramos.n@gmail.com>
2025-08-05 17:01:15 +0200

    Add the basic template to App.vue

--- _dev/src/vue/admin/configurator/App.vue
+++ _dev/src/vue/admin/configurator/App.vue
@@ -1,7 +1,28 @@
 <script setup>
+import { computed, inject } from 'vue'
+
+const props = defineProps({
+  configuratorId: { type: Number, default: null },
+})
+const $t = inject('$t')
+
+const isNewConfigurator = computed(() => !props.configuratorId)
+const pageTitle = computed(() =>
+  isNewConfigurator.value ? $t('Create a scenario') : $t('Edit the scenario'),
+)
 </script>
 
 <template>
+  <div>
+    <div class="card">
+      <div class="card-header">
+        <h1>{{ pageTitle }}</h1>
+      </div>
+      <div class="card-body">
+        <Transition name="fade" mode="out-in"> TODO </Transition>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped lang="scss"></style>
