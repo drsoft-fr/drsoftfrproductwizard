7a097ab21f65cd44ede5108482244736c5af061c
Dylan Ramos <dylan.ramos.n@gmail.com>
2025-08-05 19:09:17 +0200

    Creation of the StepList.vue component

---
+++ _dev/src/vue/admin/configurator/components/step/StepList.vue
@@ -0,0 +1,18 @@
+<script setup>
+const $t = inject('$t')
+</script>
+
+<template>
+  <div class="steps-container">
+    <h2 class="mb-3">{{ $t('Steps in the scenario') }}</h2>
+    <div class="mb-3">
+      <div class="alert alert-info">
+        <i class="material-icons">info</i>
+        {{ $t('No steps defined for this scenario.') }}
+      </div>
+      <!--      TODO rajouter la boucle sur Step -->
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
