12889b91b9ba02f91c2ebeae71f093ec30fb47ba
Dylan Ramos <dylan.ramos.n@gmail.com>
2025-08-05 19:14:23 +0200

    Retrieving the number of steps in the StepList component

--- _dev/src/vue/admin/configurator/components/step/StepList.vue
+++ _dev/src/vue/admin/configurator/components/step/StepList.vue
@@ -1,5 +1,14 @@
 <script setup>
+import { computed } from 'vue'
+import { useConfiguratorStore } from '@/js/admin/configurator/form/stores/configurator'
+import StepItem from './StepItem.vue'
+
 const $t = inject('$t')
+
+const store = useConfiguratorStore()
+
+const steps = computed(() => store.steps || [])
+const hasSteps = computed(() => steps.value.length > 0)
 </script>
 
 <template>
@@ -7,6 +16,7 @@ const $t = inject('$t')
     <h2 class="mb-3">{{ $t('Steps in the scenario') }}</h2>
     <div class="mb-3">
       <div class="alert alert-info">
+      <div v-if="!hasSteps" class="alert alert-info">
         <i class="material-icons">info</i>
         {{ $t('No steps defined for this scenario.') }}
       </div>
