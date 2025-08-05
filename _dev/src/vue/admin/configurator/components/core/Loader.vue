bbaef4acef5361d382d32e9db9a9ec2df9b0a2eb
Dylan Ramos <dylan.ramos.n@gmail.com>
2025-08-05 17:04:06 +0200

    Creating the Loader.vue component

---
+++ _dev/src/vue/admin/configurator/components/core/Loader.vue
@@ -0,0 +1,82 @@
+<script setup>
+import { computed, inject } from 'vue'
+
+const props = defineProps({
+  fullscreen: { type: Boolean, default: false },
+  size: { type: String, default: 'md' },
+  text: { type: String, default: null },
+  showText: { type: Boolean, default: true },
+})
+
+const $t = inject('$t')
+
+const spinnerClass = computed(() => {
+  const classes = ['spinner-border']
+
+  switch (props.size) {
+    case 'sm':
+      classes.push('spinner-border-sm')
+
+      break
+    case 'lg':
+      classes.push('spinner-border-lg')
+
+      break
+    default:
+      break
+  }
+
+  return classes.join(' ')
+})
+
+const loaderClass = computed(() => {
+  return props.fullscreen ? 'loader-fullscreen' : 'loader-container'
+})
+
+const computedText = computed(() => {
+  return props.text || $t('Loading...')
+})
+</script>
+
+<template>
+  <div :class="loaderClass">
+    <div class="loader-content">
+      <div :class="spinnerClass" role="status">
+        <span class="sr-only">{{ computedText }}</span>
+      </div>
+      <p v-if="showText" class="mt-3">{{ computedText }}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.loader-container {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(255, 255, 255, 0.75);
+  z-index: 1000;
+}
+
+.loader-fullscreen {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(255, 255, 255, 0.75);
+  z-index: 9999;
+}
+
+.loader-content {
+  text-align: center;
+}
+</style>
