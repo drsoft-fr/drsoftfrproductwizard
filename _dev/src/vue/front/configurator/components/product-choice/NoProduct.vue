aadb34bc7c41ca3bc3e4009293cec58d166b3b9c
Dylan Ramos <dylan.ramos.n@gmail.com>
2025-09-12 02:27:41 +0200

    Refactor NoProduct component to use consistent TailwindCSS utilities for styling

--- _dev/src/vue/front/configurator/components/product-choice/NoProduct.vue
+++ _dev/src/vue/front/configurator/components/product-choice/NoProduct.vue
@@ -10,12 +10,9 @@ const emit = defineEmits(['onSelect'])
 </script>
 
 <template>
-  <div class="card">
-    <div class="img-placeholder-container">
-      <div class="img-placeholder"></div>
-    </div>
-    <div class="card-body">
-      <h4 class="card-text">{{ choice.label }}</h4>
+  <div class="drpw:card drpw:bg-base-100 drpw:shadow-sm">
+    <div class="drpw:card-body">
+      <h4 class="drpw:card-title">{{ choice.label }}</h4>
 
       <div
         v-if="choice.description"
@@ -30,25 +27,4 @@ const emit = defineEmits(['onSelect'])
   </div>
 </template>
 
-<style scoped lang="scss">
-.card {
-  height: 100%;
-}
-
-.card-body {
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  height: 100%;
-}
-
-.img-placeholder-container {
-  background-color: var(--bs-dark-bg-subtle);
-  height: 100%;
-}
-
-.img-placeholder {
-  background: var(--bs-gradient);
-  height: 100%;
-}
-</style>
+<style scoped lang="scss"></style>
