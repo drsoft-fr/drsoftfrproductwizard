8d8efba2773f1d6afde895863e4b2ecd2f60e384
Dylan Ramos <dylan.ramos.n@gmail.com>
2025-09-12 02:40:19 +0200

    Refactor Product component to use consistent TailwindCSS utilities for styling

--- _dev/src/vue/front/configurator/components/product-choice/Product.vue
+++ _dev/src/vue/front/configurator/components/product-choice/Product.vue
@@ -20,79 +20,62 @@ provide('matchedCombination', matchedCombination)
 </script>
 
 <template>
-  <div class="card">
-    <img
-      :src="
-        matchedCombination?.imageUrl
-          ? matchedCombination.imageUrl
-          : choice.product.images[0]
-            ? choice.product.images[0].medium.url
-            : noPictureImage.medium.url
-      "
-      :alt="choice.product.name"
-      class="card-img-top"
-    />
-    <div class="card-body">
-      <h4 class="card-title">{{ choice.product.name }}</h4>
+  <div class="drpw:card drpw:bg-base-100 drpw:shadow-sm drpw:h-full">
+    <figure>
+      <img
+        :src="
+          matchedCombination?.imageUrl
+            ? matchedCombination.imageUrl
+            : choice.product.images[0]
+              ? choice.product.images[0].medium.url
+              : noPictureImage.medium.url
+        "
+        :alt="choice.product.name"
+      />
+    </figure>
+    <div class="drpw:card-body">
+      <h4 class="drpw:card-title">{{ choice.product.name }}</h4>
 
-      <p
-        v-if="true === choice.has_discount"
-        class="card-text product-price-without-reduction"
-      >
-        {{
-          formatPrice(
-            (choice.regular_price_amount || 0) +
-              (matchedCombination ? Number(matchedCombination.price || 0) : 0),
-          )
-        }}
-      </p>
+      <div class="drpw:grow">
+        <p
+          v-if="true === choice.has_discount"
+          class="drpw:text-base-content/50 drpw:line-through"
+        >
+          {{
+            formatPrice(
+              (choice.regular_price_amount || 0) +
+                (matchedCombination
+                  ? Number(matchedCombination.price || 0)
+                  : 0),
+            )
+          }}
+        </p>
 
-      <p class="card-text product-price">
-        {{
-          formatPrice(
-            (choice.price_amount || 0) +
-              (matchedCombination ? Number(matchedCombination.price || 0) : 0),
-          )
-        }}
-      </p>
+        <p class="drpw:text-accent drpw:font-bold drpw:text-xl">
+          {{
+            formatPrice(
+              (choice.price_amount || 0) +
+                (matchedCombination
+                  ? Number(matchedCombination.price || 0)
+                  : 0),
+            )
+          }}
+        </p>
+      </div>
 
       <div
         v-if="choice.description"
         v-html="choice.description"
-        class="mt-3"
+        class="drpw:mt-3 drpw:grow"
       ></div>
 
       <AttributeSelector :choice />
+
       <Quantity :choice />
+
       <Action @click="$emit('onSelect', choice)" />
     </div>
   </div>
 </template>
 
-<style scoped lang="scss">
-.card {
-  height: 100%;
-}
-
-.card-body {
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  height: 100%;
-}
-
-.card-title {
-  flex-grow: 1;
-}
-
-.product-price {
-  color: var(--bs-primary);
-  font-size: 1.2rem;
-  font-weight: bold;
-}
-
-.product-price-without-reduction {
-  color: var(--bs-secondary);
-  text-decoration: line-through;
-}
-</style>
+<style scoped lang="scss"></style>
